79ecdeca0c93aaf6989196448ee40ad9996c0806
Happy2018new <chenbo79800@163.com>
2025-08-26 09:45:30 +0800

    minor enhance

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	bdxPath := ReadStringFromPanel("请输入 BDX 文件路径: ")
 	mcworldPath := ReadStringFromPanel("请输入 mcworld 文件路径: ")
 
+	fmt.Println("转换正在进行中, 请坐与放松...")
 	err := converter.ConvertBDXToMCWorld(bdxPath, mcworldPath)
 	if err != nil {
 		fmt.Printf("处理时发生错误, 原因是: %v", err)
